4ed88c4c8f65b838f09db8013bf34187e1ec4d02
Brian Picciano <mediocregopher@gmail.com>
2014-11-30 23:53:34 -0500

    added tests for test files, fixed edtd some more

---
+++ example/example_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"io"
+	"os"
+	"github.com/stretchr/testify/require"
+	. "testing"
+
+	"github.com/mediocregopher/go.ebml/edtd"
+)
+
+var exampleFiles = []string{
+	"test.webm",
+	"test-ffmpeg.webm",
+}
+
+func TestExampleFiles(t *T) {
+
+	edtdf, err := os.Open("matroska.edtd")
+	require.Nil(t, err)
+
+	e, err := edtd.NewEdtd(edtdf)
+	require.Nil(t, err)
+
+	for _, fn := range exampleFiles {
+		f, err := os.Open(fn)
+		require.Nil(t, err, "filename: %s", fn)
+
+		p := e.NewParser(f)
+		i := 0
+		for {
+			_, err := p.Next()
+			if err == io.EOF {
+				break
+			}
+			require.Nil(t, err, "filename: %s", fn)
+			i++
+		}
+			require.NotEqual(t,0, i, "filename: %s", fn)
+	}
+}
