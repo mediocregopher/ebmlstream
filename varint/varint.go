77448f82120c202324be8f6bf3807fa8c8e0e17f
Brian Picciano <mediocregopher@gmail.com>
2014-12-05 00:03:59 -0500

    add WriteVarInt

--- varint/varint.go
+++ varint/varint.go
@@ -56,3 +56,49 @@ func VarInt(b []byte) (int64, error) {
 	buf := bytes.NewBuffer(b)
 	return ReadVarInt(buf)
 }
+
+func WriteVarInt(i int64, w io.Writer) (int, error) {
+
+	// Count the number of bits actually being used by the number
+	bits := 0
+	for ic := i; ; {
+		ic >>= 1
+		bits++
+		if ic == 0 {
+			break
+		}
+	}
+
+	// Find the number of bytes and nibbles the number uses. We round up using
+	// the cool addition hack
+	bytes := byte(bits + 7) / 8
+	nibbles := byte(bits + 3) / 4
+
+	// If the number looks like [0000 xxxx xxxx xxxx] we can stick the size bits
+	// in those first nibble, so we decrement the position they will be put in.
+	// Otherwise, we have to prepend an additional byte in the front, so we add
+	// to the bytes to account for the additional byte needing to be read
+	if nibbles % 2 != 0 {
+		nibbles--
+	} else {
+		bytes++
+	}
+
+	one := 0x80 >> (bytes - 1)
+	shifted := int64(one) << ((nibbles) * 4)
+	newI := i | shifted
+
+	out := make([]byte, 0, 8)
+	var foundData bool
+	for shift := byte(7); shift > 0; shift-- {
+		b := byte(newI >> (shift * 8))
+		if !foundData && b == 0 {
+			continue
+		}
+		foundData = true
+		out = append(out, b)
+	}
+	out = append(out, byte(newI))
+
+	return w.Write(out)
+}
--- varint/varint_test.go
+++ varint/varint_test.go
@@ -3,6 +3,7 @@ package varint
 import (
 	"bytes"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	. "testing"
 )
 
@@ -31,21 +32,37 @@ func sb(bs ...byte) string {
 	return string(b)
 }
 
-func TestReadVarInt(t *T) {
-	m := map[string]int64{
-		sb(0x81):                                           0x01,
-		sb(0xC1):                                           0x41,
-		sb(0x40, 0x01):                                     0x01,
-		sb(0x41, 0x21):                                     0x0121,
-		sb(0x20, 0x41, 0x21):                               0x4121,
-		sb(0x23, 0x41, 0x21):                               0x034121,
-		sb(0x01, 0x41, 0x21, 0x12, 0x34, 0x56, 0x78, 0x9a): 0x4121123456789a,
-	}
+var m = map[string]int64{
+	sb(0x81):                                           0x01,
+	sb(0xC1):                                           0x41,
+	sb(0x40, 0x01):                                     0x01,
+	sb(0x41, 0x21):                                     0x0121,
+	sb(0x20, 0x41, 0x21):                               0x4121,
+	sb(0x23, 0x41, 0x21):                               0x034121,
+	sb(0x01, 0x41, 0x21, 0x12, 0x34, 0x56, 0x78, 0x9a): 0x4121123456789a,
+	sb(0x01, 0x01, 0x21, 0x12, 0x34, 0x56, 0x78, 0x9a): 0x0121123456789a,
+}
 
+func TestReadVarInt(t *T) {
 	assert := assert.New(t)
 	for in, out := range m {
 		i, err := ReadVarInt(bytes.NewBuffer([]byte(in)))
-		assert.Nil(err, "input: %x", in)
-		assert.Equal(out, i, "input: %x", in)
+		assert.Nil(err, "input: 0x%x", in)
+		assert.Equal(out, i, "input: 0x%x", in)
+	}
+}
+
+func TestWriteVarInt(t *T) {
+	assert := assert.New(t)
+	for _, in := range m {
+		w := bytes.NewBuffer([]byte{})
+		_, err := WriteVarInt(in, w)
+		require.Nil(t, err, "input: 0x%x", in)
+
+		b := w.Bytes()
+		out, err := VarInt(b)
+		require.Nil(t, err, "input: 0x%x", in)
+
+		assert.Equal(in, out, "input 0x%x out; 0x%x", in, out)
 	}
 }
